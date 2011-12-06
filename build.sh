cf57aaa30f76cc5530739568f1f36e3f53091c24
Dan Newcome <dnewcome@elevenprime.com>
2011-12-06 11:53:30 -0800

    added rect.js to the build script, rebuilt donatello-min.js

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat donatello.js circle.js ellipse.js arc.js line.js | uglifyjs > build/donatello-min.js
+cat donatello.js rect.js circle.js ellipse.js arc.js line.js | uglifyjs > build/donatello-min.js
