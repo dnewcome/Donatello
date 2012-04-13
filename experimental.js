65311447e96682da3af88c02644aed42da772ae7
Dan Newcome <dnewcome@elevenprime.com>
2012-04-12 18:02:29 -0700

    removed text from experimental

--- experimental.js
+++ experimental.js
@@ -146,17 +146,6 @@ Donatello.prototype.stop = function( time, attrs ) {
 	this.attr( {'MozTransition':''});
 }
 
-/**
-* Draw text to the scene using a <div> tag.
-*/
-Donatello.prototype.text = function( x, y, str, a ) {
-	var el = Donatello.createElement( x, y, null, null, 'div');
-	el.innerHTML = str;
-	this.dom.appendChild( el );
-	var don = new Donatello( el );
-	don.attr( a );
-	return don;
-}
 
 /**
 * Draw an image to the scene using an <img> tag.
