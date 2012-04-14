bc35b620171e50c44fe5e7e14a1fb63c41300c4a
Dan Newcome <dnewcome@elevenprime.com>
2012-04-14 03:40:41 -0700

    removed some dead code in text.js

--- text.js
+++ text.js
@@ -1,12 +1,3 @@
-Donatello.prototype.text = function( x, y, str, a ) {
-	var el = Donatello.createElement( x, y, null, null, 'div');
-	el.innerHTML = str;
-	this.dom.appendChild( el );
-	var don = new Donatello( el );
-	don.attr( a );
-	return don;
-}
-
 /**
 * Draw text to the scene using a <div> tag.
 */
